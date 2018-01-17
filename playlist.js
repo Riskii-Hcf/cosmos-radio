056c9a56bfa4a345b79134d4b936cf6e82a5096e
Telkenes <taka.inzori@yahoo.com>
2018-01-17 14:50:51 +0700

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,32 @@
+exports.list = ["https://www.youtube.com/watch?v=rGlEZpOVjGo", //whatever it takes - imagine dragons
+"https://www.youtube.com/watch?v=6RLLOEzdxsM", //all falls down - alan walker
+"https://www.youtube.com/watch?v=wJnBTPUQS5A", //the spectre - alan walker
+"https://www.youtube.com/watch?v=fwUqWgHMlyo", //sky - alan walker
+"https://www.youtube.com/watch?v=1-xGerv5FOk", //alone - alan walker
+"https://www.youtube.com/watch?v=60ItHLz5WEA", //faded - alan walker
+"https://www.youtube.com/watch?v=g4hGRvs6HHU", //tired - alan walker
+"https://www.youtube.com/watch?v=PUpS-ZrrZnU", //on that day
+"https://www.youtube.com/watch?v=mWFaxs-KvMY", //find you - nick jonas
+"https://www.youtube.com/watch?v=2i2khp_npdE", //sing me to sleep - alan walker
+"https://www.youtube.com/watch?v=ALZHF5UqnU4", //alone - marshmello
+"https://www.youtube.com/watch?v=tk36ovCMsU8", //silence - marshmello
+"https://www.youtube.com/watch?v=xdYFuCp3m9k", //bad at love - halsey
+"https://www.youtube.com/watch?v=-grLLLTza6k", //wolves - marshmello/selena gomez
+"https://www.youtube.com/watch?v=FM7MFYoylVs", //just something like this
+"https://www.youtube.com/watch?v=fvrXjKXhLEs", //Once in a while - timeflies
+"https://www.youtube.com/watch?v=IhP3J0j9JmY", //beliver - imagine dragons
+"https://www.youtube.com/watch?v=E6RBTPMFATk", //radioactive - imagine dragons
+"https://www.youtube.com/watch?v=hhSA9H9Iaqw", //monster - imagine dragons
+"https://www.youtube.com/watch?v=h--P8HzYZ74", //stay - zed
+"https://www.youtube.com/watch?v=BgSCbJ5uCKg", //stargazing
+"https://www.youtube.com/watch?v=0501BTnbrxg", //let you down - NF
+"https://www.youtube.com/watch?v=AjWAGuFh6M4", //Wow theme song
+"https://www.youtube.com/watch?v=Mw9U7FPaZho", //rountine - alan walker
+"https://www.youtube.com/watch?v=rkVZtZuMd8Y", //anywhere - rita ora
+"https://www.youtube.com/watch?v=XCWmONajkOg", //let me go
+"https://www.youtube.com/watch?v=Sf4BgSLPQKY", //rich love - one republic
+"https://www.youtube.com/watch?v=mlaxs1Ur-NU", //135 - alan walker
+"https://www.youtube.com/watch?v=lqYQXIt4SpA", //force - alan walker
+"https://www.youtube.com/watch?v=t8U-l4PbdNw", //him and I
+"https://www.youtube.com/watch?v=feA64wXhbjo", //shooting stars
+];
